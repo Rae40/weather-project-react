983a6bf330b5e99124b3270dd31da0994690b88e
Rae40 <103543425+Rae40@users.noreply.github.com>
2022-09-05 20:53:13 -0400

    added api integration

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setWeatherData]= useState({ready: false});
   function handleResponse (response){
     setWeatherData({
@@ -82,8 +82,7 @@ export default function Weather() {
   } else {
 
   const apiKey= "500b3e347fb42fdce954cc2ffb572cb8";
-  let city = "New York";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}}&appid=${apiKey}&units=metrics`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}}&appid=${apiKey}&units=metrics`;
   axios.get(apiUrl).then(handleResponse);
 
   return "Loading....";
