69a03d45b0db369432b3744a1ba0dcda654bdf5d
Rae40 <103543425+Rae40@users.noreply.github.com>
2022-09-22 20:27:18 -0400

    fixed api error

--- src/Weather.js
+++ src/Weather.js
@@ -24,7 +24,7 @@ export default function Weather(props) {
   }
   function search() {
     const apiKey = "500b3e347fb42fdce954cc2ffb572cb8";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metrics`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
