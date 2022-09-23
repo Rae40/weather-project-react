5d3cab7e5c43ecdc0c268969dfdbed564c2b16d1
Rae40 <103543425+Rae40@users.noreply.github.com>
2022-09-22 20:35:26 -0400

    fixed name display error

--- src/Weather.js
+++ src/Weather.js
@@ -19,7 +19,7 @@ export default function Weather(props) {
       description: response.data.weather[0].icon,
       icon: response.data.weather[0].icon,
       wind: response.data.wind.speed,
-      city: response.data.main.name,
+      city: response.data.name,
     });
   }
   function search() {
